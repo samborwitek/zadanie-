afabcf94538bcc0110085448ff0fc6ddc1d9f279
Marek Zatwarnicki <73995@ue.poznan.pl>
2023-06-26 02:17:36 +0200

    Edytowano plik main

---
+++ main.py
@@ -0,0 +1,11 @@
+import math
+
+def oblicz_objetosc_szescianu(a):
+    return a**3
+
+def oblicz_pole_szescianu(a):
+    return 6 * a**2
+
+def oblicz_objetosc_kuli(r):
+    return (4/3) * math.pi * r**3
+
